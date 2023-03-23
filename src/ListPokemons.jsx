7c4aba2e47ee4a906ebed5802b915fd74c6dc311
fleonmendza <fleonmendza@gmail.com>
2023-03-23 17:56:57 -0600

    CreateProyect

---
+++ src/ListPokemons.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import {getApiPoke, getInfoPokemon} from './pokeApi'
+
+
+// getApiPoke()
+
+const [pokeDatos, setpokeDatos] = useState(getApiPoke())
+
+
+
+export const ListPokemons = () => {
+ 
+    return (
+        <>bjjb</>
+  )
+}
